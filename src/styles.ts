af8a0448000c56a149722a7affa57535840cb535
Simon Spies <simon.spies@sisesp.de>
2026-07-20 15:19:29 +0200

    Update to and adjustements for newer apexcharts

--- src/styles.ts
+++ src/styles.ts
@@ -308,64 +308,70 @@ export const stylesApex: CSSResultGroup = css`
     color: inherit;
   }
 
-  .apexcharts-tooltip-marker::before {
-    content: '';
-    display: inline-block;
-    width: 100%;
-    text-align: center;
-    color: currentcolor;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    font-size: 26px;
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 14px;
-    font-weight: 900;
+  .apexcharts-tooltip-marker::before,
+  .apexcharts-tooltip-marker::after {
+    content: none;
+    display: none;
   }
 
-  .apexcharts-tooltip-marker[shape='circle']::before {
-    content: '\\25CF';
-  }
+  //.apexcharts-tooltip-marker::before {
+  //  content: '';
+  //  display: inline-block;
+  //  width: 100%;
+  //  text-align: center;
+  //  color: currentcolor;
+  //  text-rendering: optimizeLegibility;
+  //  -webkit-font-smoothing: antialiased;
+  //  font-size: 26px;
+  //  font-family: Arial, Helvetica, sans-serif;
+  //  line-height: 14px;
+  //  font-weight: 900;
+  //}
 
-  .apexcharts-tooltip-marker[shape='square']::before,
-  .apexcharts-tooltip-marker[shape='rect']::before {
-    content: '\\25A0';
-    transform: translate(-1px, -2px);
-  }
+  //.apexcharts-tooltip-marker[shape='circle']::before {
+  //  content: '\\25CF';
+  //}
 
-  .apexcharts-tooltip-marker[shape='line']::before {
-    content: '\\2500';
-  }
+  //.apexcharts-tooltip-marker[shape='square']::before,
+  //.apexcharts-tooltip-marker[shape='rect']::before {
+  //  content: '\\25A0';
+  //  transform: translate(-1px, -2px);
+  //}
 
-  .apexcharts-tooltip-marker[shape='diamond']::before {
-    content: '\\25C6';
-    font-size: 28px;
-  }
+  //.apexcharts-tooltip-marker[shape='line']::before {
+  //  content: '\\2500';
+  //}
 
-  .apexcharts-tooltip-marker[shape='triangle']::before {
-    content: '\\25B2';
-    font-size: 22px;
-  }
+  //.apexcharts-tooltip-marker[shape='diamond']::before {
+  //  content: '\\25C6';
+  //  font-size: 28px;
+  //}
 
-  .apexcharts-tooltip-marker[shape='cross']::before {
-    content: '\\2715';
-    font-size: 18px;
-  }
+  //.apexcharts-tooltip-marker[shape='triangle']::before {
+  //  content: '\\25B2';
+  //  font-size: 22px;
+  //}
 
-  .apexcharts-tooltip-marker[shape='plus']::before {
-    content: '\\2715';
-    transform: rotate(45deg) translate(-1px, -1px);
-    font-size: 18px;
-  }
+  //.apexcharts-tooltip-marker[shape='cross']::before {
+  //  content: '\\2715';
+  //  font-size: 18px;
+  //}
 
-  .apexcharts-tooltip-marker[shape='star']::before {
-    content: '\\2605';
-    font-size: 18px;
-  }
+  //.apexcharts-tooltip-marker[shape='plus']::before {
+  //  content: '\\2715';
+  //  transform: rotate(45deg) translate(-1px, -1px);
+  //  font-size: 18px;
+  //}
 
-  .apexcharts-tooltip-marker[shape='sparkle']::before {
-    content: '\\2726';
-    font-size: 20px;
-  }
+  //.apexcharts-tooltip-marker[shape='star']::before {
+  //  content: '\\2605';
+  //  font-size: 18px;
+  //}
+
+  //.apexcharts-tooltip-marker[shape='sparkle']::before {
+  //  content: '\\2726';
+  //  font-size: 20px;
+  //}
 
   .apexcharts-tooltip-series-group {
     padding: 0 10px;
